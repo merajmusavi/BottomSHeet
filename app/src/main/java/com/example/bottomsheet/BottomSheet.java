91dfd1793907380ab48758caf49dc06059bbe821
merajuni7ed <merajmusavi1243m@gmail.com>
2022-12-05 18:02:57 +0330

    Initial commit

---
+++ app/src/main/java/com/example/bottomsheet/BottomSheet.java
@@ -0,0 +1,27 @@
+package com.example.bottomsheet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheet extends BottomSheetDialogFragment {
+    Button confirm,cancel;
+    EditText et_username,et_password;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottomsheet,container,false);
+        confirm = view.findViewById(R.id.confirm)
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+
+}
