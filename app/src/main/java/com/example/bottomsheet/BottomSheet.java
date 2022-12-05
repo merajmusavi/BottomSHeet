1d21827a38d846dc8bc6c4660a62892ba3f4b473
merajuni7ed <merajmusavi1243m@gmail.com>
2022-12-05 18:26:18 +0330

    last update

--- app/src/main/java/com/example/bottomsheet/BottomSheet.java
+++ app/src/main/java/com/example/bottomsheet/BottomSheet.java
@@ -1,11 +1,14 @@
 package com.example.bottomsheet;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,15 +16,49 @@ import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BottomSheet extends BottomSheetDialogFragment {
-    Button confirm,cancel;
-    EditText et_username,et_password;
+    Button confirm, cancel;
+    EditText et_username, et_password;
+
+    CallBack callBack;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        callBack = (CallBack) context;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.bottomsheet,container,false);
-        confirm = view.findViewById(R.id.confirm)
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.bottomsheet, container, false);
+        et_password = view.findViewById(R.id.editText2);
+        et_username = view.findViewById(R.id.editText);
+        cancel = view.findViewById(R.id.cancel_button);
+        confirm = view.findViewById(R.id.confirm_button);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (et_username.length() > 0) {
+                    callBack.username(et_username.getText().toString());
+                    dismiss();
+                }else {
+                    Toast.makeText(getContext(), "username cant be empty", Toast.LENGTH_SHORT).show();
+                }
 
+            }
+        });
+
+
+        return view;
     }
 
+    public interface CallBack {
+        void username(String username);
+    }
 }
--- app/src/main/java/com/example/bottomsheet/MainActivity.java
+++ app/src/main/java/com/example/bottomsheet/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.bottomsheet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements BottomSheet.CallBack {
+BottomSheet bottomSheet;
+Button button;
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomSheet = new BottomSheet();
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheet.show(getSupportFragmentManager(),null);
+            }
+        });
+
+    }
+
+    @Override
+    public void username(String username) {
+        textView.setText(username);
     }
 }
